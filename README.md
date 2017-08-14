b9f8982d3a052521942bf4daa11be1540afc6fa9
alickzhang <alick369@gmail.com>
2017-08-15 00:23:18 +1000

    update README.md & implement change play mode

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # React Responsive Music Player
+
+## Example
+
+Please see a demo here.
+
+## Install
+
+``` bash
+npm install react-responsive-music-player --save
+```
+
+## Usage
+
+``` jsx
+import MusicPlayer from 'react-responsive-music-player'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        ...
+        <MusicPlayer playlist={playlist} />
+        ...
+      </div>
+    )
+  }
+}
+```
+
+## API
+
+### props
+
+prop       | type   | default | notes
+-----------|--------|---------|--------
+autoplay   | bool   | false   |
+playlist   | object | []      | the playlist
+style      | object | {}      |
+themeColor | string | #66cccc | the theme color of your app
+
+### JSON
+
+``` json
+const playlist = [
+  {
+    url: 'path/to/mp3',
+    cover: 'path/to/jpg',
+    title: 'Despacito',
+    artist: [
+      'Luis Fonsi',
+      'Daddy Yankee'
+    ]
+  },
+  {
+    url: 'path/to/mp3',
+    cover: 'path/to/jpg',
+    title: 'Bedtime Stories',
+    artist: [
+      'Jay Chou'
+    ]
+  }
+]
+```
+
+## Development
+
+``` bash
+npm install
+npm start
+```
+
+## License
+
+MIT
--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -3,16 +3,32 @@ import PropTypes from 'prop-types'
 import './MusicPlayer.css'
 
 class MusicPlayer extends Component {
+
+  static propTypes = {
+    autoplay: PropTypes.bool,
+    playlist: PropTypes.array.isRequired,
+    style: PropTypes.object,
+    themeColor: PropTypes.string
+  };
+
+  static defaultProps = {
+    autoplay: false,
+    playlist: [],
+    style: {},
+    themeColor: '#66cccc'
+  }
+
   constructor(props) {
     super(props)
     this.state = {
       activeMusicIndex: 0,
       leftTime: 0,
       play: this.props.autoplay || false,
+      playMode: 'loop',
       progress: 0,
-      repeat: false,
       volume: 1
     }
+    this.modeList = ['loop', 'random', 'repeat']
   }
 
   componentDidMount() {
@@ -27,7 +43,34 @@ class MusicPlayer extends Component {
     audioContainer.removeEventListnere('ended', this.end.bind(this))
   }
 
-  adjustProgress(e) {
+  updateProgress() {
+    const duration = this.audioContainer.duration
+    const currentTime = this.audioContainer.currentTime
+    const progress = currentTime / duration
+    this.setState({
+      progress: progress,
+      leftTime: duration - currentTime
+    })
+  }
+
+  end() {
+    const { playMode, activeMusicIndex } = this.state
+    if (playMode === 'repeat') {
+      this.audioContainer.play()
+    } else if (playMode === 'loop') {
+      this.handleNext()
+    } else if (playMode === 'random') {
+      let randomIndex = Math.floor(Math.random() * this.props.playlist.length)
+      while (randomIndex === activeMusicIndex) {
+        randomIndex = Math.floor(Math.random() * this.props.playlist.length)
+      }
+      this._playMusic(randomIndex)
+    } else {
+      this.setState({ play: false })
+    }
+  }
+
+  handleAdjustProgress(e) {
     const progressContainer = this.progressContainer
     const progress = (e.clientX - progressContainer.getBoundingClientRect().left) / progressContainer.clientWidth
     const currentTime = this.audioContainer.duration * progress
@@ -40,17 +83,7 @@ class MusicPlayer extends Component {
     })
   }
 
-  updateProgress() {
-    const duration = this.audioContainer.duration
-    const currentTime = this.audioContainer.currentTime
-    const progress = currentTime / duration
-    this.setState({
-      progress: progress,
-      leftTime: duration - currentTime
-    })
-  }
-
-  adjustVolume(e) {
+  handleAdjustVolume(e) {
     const volumeContainer = this.volumeContainer
     let volume = (e.clientX - volumeContainer.getBoundingClientRect().left) / volumeContainer.clientWidth
     volume = volume < 0 ? 0 : volume
@@ -60,16 +93,30 @@ class MusicPlayer extends Component {
     })
   }
 
-  toggle() {
+  handleToggle() {
     this.state.play ? this.audioContainer.pause() : this.audioContainer.play()
     this.setState({ play: !this.state.play })
   }
 
-  end() {
-    this.state.repeat ? this.audioContainer.play() : this.props.autoplay ? this.next() : this.setState({ play: false })
+  handlePrev() {
+    const total = this.props.playlist.length
+    const activeMusicIndex = this.state.activeMusicIndex > 0 ? this.state.activeMusicIndex - 1 : total - 1
+    this._playMusic(activeMusicIndex)
   }
 
-  playMusic(index) {
+  handleNext() {
+    const total = this.props.playlist.length
+    const activeMusicIndex = this.state.activeMusicIndex < total - 1 ? this.state.activeMusicIndex + 1 : 0
+    this._playMusic(activeMusicIndex)
+  }
+
+  handleChangePlayMode() {
+    let index = this.modeList.indexOf(this.state.playMode)
+    index = (index + 1) % this.modeList.length
+    this.setState({ playMode: this.modeList[index] })
+  }
+
+  _playMusic(index) {
     this.setState({
       activeMusicIndex: index,
       leftTime: 0,
@@ -80,23 +127,7 @@ class MusicPlayer extends Component {
     })
   }
 
-  prev() {
-    const total = this.props.playlist.length
-    const activeMusicIndex = this.state.activeMusicIndex > 0 ? this.state.activeMusicIndex - 1 : total - 1
-    this.playMusic(activeMusicIndex)
-  }
-
-  next() {
-    const total = this.props.playlist.length
-    const activeMusicIndex = this.state.activeMusicIndex < total - 1 ? this.state.activeMusicIndex + 1 : 0
-    this.playMusic(activeMusicIndex)
-  }
-
-  changePlayMode() {
-
-  }
-
-  formatTime(time) {
+  _formatTime(time) {
     if (isNaN(time) || time === 0) {
       return
     }
@@ -105,9 +136,14 @@ class MusicPlayer extends Component {
     return `${mins < 10 ? '0' : ''}${mins}:${secs < 10 ? '0' : ''}${secs}`
   }
 
+  _processArtistName(artistList) {
+    return artistList.join(' / ')
+  }
+
   render() {
-    const { activeMusicIndex } = this.state
+    const { activeMusicIndex, playMode } = this.state
     const activeMusic = this.props.playlist[activeMusicIndex]
+    const playModeClass = playMode === 'loop' ? 'refresh' : playMode === 'random' ? 'random' : 'repeat'
 
     return (
       <div className="player-container" style={this.props.style}>
@@ -120,10 +156,10 @@ class MusicPlayer extends Component {
         <div className="info-and-control">
           <div className="music-info">
             <h2 className="title">{activeMusic.title}</h2>
-            <h3 className="artist">{activeMusic.artist}</h3>
+            <h3 className="artist">{this._processArtistName(activeMusic.artist)}</h3>
           </div>
           <div className="time-and-volume">
-            <div className="left-time">-{this.formatTime(this.state.leftTime)}</div>
+            <div className="left-time">-{this._formatTime(this.state.leftTime)}</div>
             <div className="volume-container">
               <div className="volume-icon">
                 <i className="icon fa fa-volume-up"></i>
@@ -131,7 +167,7 @@ class MusicPlayer extends Component {
               <div className="volume-wrapper">
                 <div
                   className="progress-container"
-                  onClick={this.adjustVolume.bind(this)}
+                  onClick={this.handleAdjustVolume.bind(this)}
                   ref={(ref) => { this.volumeContainer = ref }}
                 >
                   <div className="progress" style={{width: `${this.state.volume * 100}%`}}></div>
@@ -141,19 +177,19 @@ class MusicPlayer extends Component {
           </div>
           <div
             className="progress-container"
-            onClick={this.adjustProgress.bind(this)}
+            onClick={this.handleAdjustProgress.bind(this)}
             ref={(ref) => { this.progressContainer = ref }}
           >
             <div className="progress" style={{width: `${this.state.progress * 100}%`, background: this.props.themeColor}}></div>
           </div>
           <div className="control-container">
             <div className="mode-control">
-              <i className="icon fa fa-random" onClick={this.changePlayMode.bind(this)}></i>
+              <i className={`icon fa fa-${playModeClass}`} onClick={this.handleChangePlayMode.bind(this)}></i>
             </div>
             <div className="controls">
-              <i className="icon fa fa-step-backward" onClick={this.prev.bind(this)}></i>
-              <i className={`icon fa fa-${this.state.play ? 'pause' : 'play'}`} onClick={this.toggle.bind(this)}></i>
-              <i className="icon fa fa-step-forward" onClick={this.next.bind(this)}></i>
+              <i className="icon fa fa-step-backward" onClick={this.handlePrev.bind(this)}></i>
+              <i className={`icon fa fa-${this.state.play ? 'pause' : 'play'}`} onClick={this.handleToggle.bind(this)}></i>
+              <i className="icon fa fa-step-forward" onClick={this.handleNext.bind(this)}></i>
             </div>
           </div>
         </div>
@@ -163,20 +199,6 @@ class MusicPlayer extends Component {
       </div>
     )
   }
-}
-
-MusicPlayer.defaultProps = {
-  autoplay: false,
-  themeColor: '#66cccc',
-  playlist: [],
-  style: {}
-}
-
-MusicPlayer.propTypes = {
-  autoplay: PropTypes.bool,
-  themeColor: PropTypes.string,
-  playlist: PropTypes.array.isRequired,
-  style: PropTypes.object
 }
 
 export default MusicPlayer
