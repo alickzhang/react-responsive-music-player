e62ac58bea3d8e0688b43f6bf2181c22407c1fc1
alickzhang <alick369@gmail.com>
2019-04-15 23:43:02 +1000

    build: upgrade packages; support eslint and stylelint

--- src/index.js
+++ src/index.js
@@ -1,50 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import MusicPlayer from './MusicPlayer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MusicPlayer from './MusicPlayer';
 
 const playlist = [
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3',
     cover: 'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
     title: 'Despacito',
-    artist: [
-      'Luis Fonsi',
-      'Daddy Yankee'
-    ]
+    artist: ['Luis Fonsi', 'Daddy Yankee']
   },
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
     cover: 'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg',
     title: 'Bedtime Stories',
-    artist: [
-      'Jay Chou'
-    ]
+    artist: ['Jay Chou']
   },
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502444212/Actor_ud8ccw.mp3',
     cover: 'http://res.cloudinary.com/alick/image/upload/v1502444304/actor_umzdur.jpg',
     title: '演员',
-    artist: [
-      '薛之谦'
-    ]
+    artist: ['薛之谦']
   },
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502444215/Bridge_of_Fate_aaksg1.mp3',
     cover: 'http://res.cloudinary.com/alick/image/upload/v1502444306/Bridge_of_Fate_o36rem.jpg',
     title: 'Bridge of Fate',
-    artist: [
-      '王力宏',
-      '谭维维'
-    ]
+    artist: ['王力宏', '谭维维']
   },
   {
     url: 'http://res.cloudinary.com/alick/video/upload/v1502444222/Goodbye_byaom5.mp3',
     cover: 'http://res.cloudinary.com/alick/image/upload/v1502444310/Goodbye_hpubmk.jpg',
     title: 'Goodbye',
-    artist: [
-      'G.E.M.'
-    ]
+    artist: ['G.E.M.']
   }
-]
+];
 
-ReactDOM.render(<MusicPlayer playlist={playlist} autoplay />, document.getElementById('root'))
+ReactDOM.render(<MusicPlayer playlist={playlist} autoplay />, document.getElementById('root'));
