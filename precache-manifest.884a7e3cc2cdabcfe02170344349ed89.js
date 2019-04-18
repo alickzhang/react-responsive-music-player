6cf35af668287b1c621b4ee36bd8786e3e75798e
alickzhang <alick369@gmail.com>
2019-04-19 00:16:18 +1000

    Updates

---
+++ precache-manifest.884a7e3cc2cdabcfe02170344349ed89.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "427b50a7be4b5a8d4983",
+    "url": "/react-responsive-music-player/static/css/main.b3ef0ae0.chunk.css"
+  },
+  {
+    "revision": "427b50a7be4b5a8d4983",
+    "url": "/react-responsive-music-player/static/js/main.64bd5f4a.chunk.js"
+  },
+  {
+    "revision": "c05e67788645e971b3c5",
+    "url": "/react-responsive-music-player/static/js/runtime~main.0c4513b0.js"
+  },
+  {
+    "revision": "bd559db0ed35aefe33f0",
+    "url": "/react-responsive-music-player/static/css/2.b84cc801.chunk.css"
+  },
+  {
+    "revision": "bd559db0ed35aefe33f0",
+    "url": "/react-responsive-music-player/static/js/2.d914bcee.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-responsive-music-player/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-responsive-music-player/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-responsive-music-player/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-responsive-music-player/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-responsive-music-player/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ed3cd406271198a9236c2736eb5ea7ce",
+    "url": "/react-responsive-music-player/index.html"
+  }
+];
